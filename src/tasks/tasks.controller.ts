27242f05235e0b387b457e202fcdd67a1cd3baf5
todorone <1@todor.one>
2019-12-26 12:00:42 +0200

    wip2

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,46 @@
-import { Controller, Get } from '@nestjs/common'
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common'
 import { TasksService } from './tasks.service'
+import { Task, TaskStatus } from './task.model'
+import { CreateTaskDto } from './dto/createTask.dto'
 
-@Controller('tasks')
+@Controller('/tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksService.getAllTasks()
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id)
+  }
+
+  @Patch('/:id/status')
+  patchTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ) {
+    console.log('status', status)
+
+    const task = this.tasksService.updateTask(id, { status })
+
+    if (!task) throw new NotFoundException()
+
+    return task
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string) {
+    const isSuccess = this.tasksService.deleteTask(id)
+
+    if (!isSuccess) throw new NotFoundException()
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto)
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import * as uuid from 'uuid/v4'
+import { Task, TaskStatus } from './task.model'
+import { CreateTaskDto } from './dto/createTask.dto'
 
 @Injectable()
 export class TasksService {
-  private tasks = []
+  private tasks: Task[] = []
 
   getAllTasks() {
     return this.tasks
+  }
+
+  getTaskById(id: string): Task {
+    const task = this.tasks.find(t => t.id === id)
+
+    if (!task) {
+      throw new NotFoundException()
+    } else return task
+  }
+
+  deleteTask(id: string): boolean {
+    const index = this.tasks.findIndex(t => t.id === id)
+
+    if (index === -1) {
+      return false
+    } else {
+      this.tasks.splice(index, 1)
+      return true
+    }
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const task: Task = {
+      id: uuid(),
+      title: createTaskDto.title,
+      description: createTaskDto.description,
+      status: TaskStatus.OPEN,
+    }
+
+    this.tasks.push(task)
+    return task
+  }
+
+  updateTask(id: string, patch: Partial<Task>): Task | null {
+    const task = this.getTaskById(id)
+
+    Object.entries(patch).forEach(([key, value]) => task[key] = value)
+
+    return task
   }
 }
