7642730191671c8f142f1aa701adf2154c830183
todorone <1@todor.one>
2019-12-27 09:51:57 +0200

    added multi-environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+require('dotenv').config()
+
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as config from 'config'
 
 async function bootstrap() {
+  const configServer = config.get('server')
+
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const port = configServer.port
+  await app.listen(port)
 }
 
 bootstrap()
